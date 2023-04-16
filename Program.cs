a20487718f5942f900e425dd0616c2520530e5e5
HHH <adilasif453@gmail.com>
2023-04-16 01:08:18 -0700

    DoublyLinkedListOperations

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DoublyLinkedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,22 +15,42 @@ namespace programs
         static void Main(string[] args)
         {
 
-            linkedlist a = new linkedlist();
 
-            a.insertlast(3, null);
 
-            a.insertlast(4, null);
-            //a.insertlast(164, null);
-            //a.insertlast(154, null);
-            //a.insertlast(114, null);
-            //a.insertlast(124, null);
-            //a.insertFirst(3, null);
-            //a.AddAfterSpecific(67, 5);
-            //a.removeAfterSpecific(124);
-            a.prinlist();
-            Console.WriteLine("\n-------------------------------------------");
-            a.PrintReverseList();
-            a.prinlist();
+            #region DoublyLinked List
+
+            DoublyLinkedLst ls = new DoublyLinkedLst();
+            ls.insertatlast(4, null, null);
+            ls.insertatlast(14, null, null);
+            ls.insertatlast(124, null, null);
+            ls.insertatlast(43, null, null);
+            ls.insertatlast(445, null, null);
+            ls.printList();
+
+            ls.DeletefromLaast();
+
+           // ls.DeleteAtSpecificPosition(2,2);
+            ls.printList();
+            #endregion
+
+            #region Singly LinkedList
+            //linkedlist a = new linkedlist();
+
+            //a.insertlast(3, null);
+
+            //a.insertlast(4, null);
+            ////a.insertlast(164, null);
+            ////a.insertlast(154, null);
+            ////a.insertlast(114, null);
+            ////a.insertlast(124, null);
+            ////a.insertFirst(3, null);
+            ////a.AddAfterSpecific(67, 5);
+            ////a.removeAfterSpecific(124);
+            //a.prinlist();
+            //Console.WriteLine("\n-------------------------------------------");
+            //a.PrintReverseList();
+            //a.prinlist();
+            #endregion
             Console.ReadLine();
 
         }
